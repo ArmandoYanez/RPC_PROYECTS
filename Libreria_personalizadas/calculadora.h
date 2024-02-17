93e02731dfea497700f6f92c6ee21d966cc47262
Esdras <esdras2israel@gmail.com>
2024-02-17 13:39:43 -0600

    ayuda

---
+++ Libreria_personalizadas/calculadora.h
@@ -0,0 +1,19 @@
+//
+// Created by Esdras on 17/02/2024.
+//
+
+#ifndef PROYECTOS_CALCULADORA_H
+#define PROYECTOS_CALCULADORA_H
+
+#include <iostream>
+int suma(){
+
+    int x1, x2;
+
+    std::cin>>x1;
+    std::cin>>x2;
+
+    return 5+20;
+}
+
+#endif //PROYECTOS_CALCULADORA_H
---
+++ Libreria_personalizadas/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Esdras on 17/02/2024.
+//
+
+#include "calculadora.h"
+
+int main(){
+
+    std::cout<<suma();
+
+    return  0;
+}
